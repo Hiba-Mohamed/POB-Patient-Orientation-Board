2a7dbe192c4d7e1e9bdb47854e0990eae2e4d3bb
Clark Oake <clarkoake13@gmail.com>
2023-03-28 17:57:13 -0230

    Cleaned up nesting in functions.js
    Fixed formatting for better readability of existing code

--- functions.js
+++ functions.js
@@ -1,35 +1,47 @@
-function clock(){
+function clock()
+{
     var date = new Date();
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
     var midday;
     
-     hours = updateTime(hours);
-     minutes = updateTime(minutes); 
-     seconds = updateTime(seconds);
+    hours = updateTime(hours);
+    minutes = updateTime(minutes); 
+    seconds = updateTime(seconds);
 
-// making the greeting message say "good morning!" from 6am until 1pm
- midday = (hours >=12) ?"PM" : "AM";
- document.getElementById("current-time").innerHTML = hours + ":" + minutes + ":" + seconds + midday
-var time = setTimeout(function(){
-    clock();
-}, 1000);
+    // making the greeting message say "good morning!" from 6am until 1pm
+
+    midday = (hours >=12) ?"PM" : "AM";
+    document.getElementById("current-time").innerHTML = hours + ":" + minutes + ":" + seconds + midday
+
+    var time = setTimeout(function(){
+        clock();
+    }, 1000);
 }
-  if( hours < 12)  {
-      var greeting = "Good Morning";
-  }
-   if(hours >+ 12 && hours <=18)  {
-       var greeting = "Good Afternoon";
-       
-    if(hours >= 18 && hours <= 24){
-     var greeting = "good evening";
-        }
+
+if( hours < 12)
+{
+    var greeting = "Good Morning";
 }
-function updateTime(k){
-    if(k <10){
+
+if(hours >+ 12 && hours <=18)
+{
+    var greeting = "Good Afternoon";
+
+    if(hours >= 18 && hours <= 24)
+    {
+        var greeting = "good evening";
+    }
+}
+
+function updateTime(k)
+{
+    if(k <10)
+    {
         return "0" + k
-    } else {
+    } else 
+    {
         return k;
     }
 }
@@ -37,18 +49,20 @@ function updateTime(k){
 clock();
 
     // creating date function
-function date(){
+function date()
+{
     let date = document.getElementsByClassName("calendar-text")[0];
     let m = new Date();
-    date.innerHTML = m.toLocaleDateString();}
+    date.innerHTML = m.toLocaleDateString();
+}
 
-    // calling date function
-     date();
+// calling date function
+date();
 
-    //  getting the name of the day of the week displayed in the date
+//  getting the name of the day of the week displayed in the date
 
 
-    // getting the name of the month of the year displayed in the date
+// getting the name of the month of the year displayed in the date
 
 
  
