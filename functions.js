032bf282719080f8f27cd0a75bb210d41fcbc738
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-03-28 16:48:13 -0230

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,14 +1,40 @@
-// creating clock function
-function clock () {
-    const time = document.getElementById("current-time");
-   setInterval(() =>{
-   const d = new Date();
-   time.innerHTML = d.toLocaleTimeString();
-   },1000)}
-   
-//    calling clock function
-    clock();
+function clock(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var midday;
     
+     hours = updateTime(hours);
+     minutes = updateTime(minutes); 
+     seconds = updateTime(seconds);
+
+// making the greeting message say "good morning!" from 6am until 1pm
+ midday = (hours >=12) ?"PM" : "AM";
+ document.getElementById("current-time").innerHTML = hours + ":" + minutes + ":" + seconds + midday
+var time = setTimeout(function(){
+    clock();
+}, 1000);
+}
+  if( hours < 12)  {
+      var greeting = "Good Morning";
+  }
+   if(hours >+ 12 && hours <=18)  {
+       var greeting = "Good Afternoon";
+       
+    if(hours >= 18 && hours <= 24){
+     var greeting = "good evening";
+        }
+}
+function updateTime(k){
+    if(k <10){
+        return "0" + k
+    } else {
+        return k;
+    }
+}
+
+clock();
 
     // creating date function
 function date(){
@@ -18,3 +44,11 @@ function date(){
 
     // calling date function
      date();
+
+    //  getting the name of the day of the week displayed in the date
+
+
+    // getting the name of the month of the year displayed in the date
+
+
+ 
