eb1e4145e2e828710d7cbdf14095151b406a63c7
Clark Oake <clarkoake13@gmail.com>
2023-04-05 18:54:30 -0230

    added helpful comments
    commented on a few issues and provided some guidance for resolving them

--- functions.js
+++ functions.js
@@ -1,10 +1,20 @@
 
-session          = document.getElementById('session');
-hours            = document.getElementById('hours');
-minutes          = document.getElementById('minutes');
-seconds          = document.getElementById('seconds');
-greeting_message = document.getElementById('greeting-text');
+// Watch out for your closures... You've got a closing brace after the 
+// line for 'var = seconds' which is closing your displayTime function
+// and causing issues throughout your code
 
+// Pay attention to how you declare variables. not putting the 'const' declarator
+// on your variables leaves them mutable and could cause issues
+const session          = document.getElementById('session');
+const hours            = document.getElementById('hours');
+const minutes          = document.getElementById('minutes');
+const seconds          = document.getElementById('seconds');
+const greeting_message = document.getElementById('greeting-text');
+
+console.log(session);
+
+// Watch out for global scope vs block scope here... You are declaring a function
+// that just declares local variables and completes without doing anything
 function displayTime() 
  {
     var dateTime     = new Date();
@@ -13,9 +23,19 @@ function displayTime()
     var seconds      = dateTime.getSeconds();
  }
 
+ // If you are trying to get your functions working but not seeing anything happening
+ // you can try invoking the function in a console log to see what it's return is
  displayTime() 
+//  console.log(displayTime())
+// In this case, the function isn't doing any work or modifying the DOM in any way
+
+// Again, watch out for how you are declaring variables here.. This may work fine but it is bad practice
+// We should always explicitly define wether a variable is mutable (let) or immutable (const)
  AM_PM = ( hours >= 12 ) ? session.innerHTML = "PM": session.innerHTML= "AM";
 
+
+ // we are delcaring this if statement in the global scope so the return value isn't being stored anywhere
+ // This is what is causing your console error for 'Illegal Return Statement"
  //  current_value is a number
  if (String(current_value).length < 2)
      {
@@ -33,7 +53,7 @@ function displayTime()
 //     current_value.toString().length <2 ? return "0" + current_value : return current_value;
 //  }
 
- 
+
  if ( hours < 12 )
  {
      greeting_message.innerHTML = "Good Morning";
