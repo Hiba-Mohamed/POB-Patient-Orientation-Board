73c9be60186dd2cdf3241ee3fcba5fcd72cb02c2
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-04-06 21:29:37 -0230

    leading zero to hours issues

--- functions.js
+++ functions.js
@@ -14,9 +14,10 @@ const calendar_text    = document.getElementById('calendar-text');
 const clock_text       = document.getElementById('clock');
 
 
-
 // Watch out for global scope vs block scope here... You are declaring a function
 // that just declares local variables and completes without doing anything
+
+// Displaying live clock on the top of the page
 function displayTime() 
  {
     var dateTime     = new Date();
@@ -30,44 +31,16 @@ function displayTime()
         clock_text.innerHTML = new_date.toLocaleTimeString();
      }
      setInterval(liveClock, 1000);
-     
-    // adding zero infront of the numbers below 10
-    
-    let current_value
-    if (String(current_value).length < 2)
-    {
-        const value = '0' + current_value;
-        console.log(typeof(value));
-        return value
-    } 
-    else 
-    {
-        return current_value;
-    }
 
-    if (hours < 12)
-     {
-        setAttribute(greeting_message.innerHTML, "good night")
-     }
-
-    // // changing the greeeting message with time change
-    // if ( hours < 12 )
-    // {
-    //    
-    //     // greeting_message.innerHTML = "Good Morning";
-    // }
-    
-    // if( hours >= 12 && hours <= 18 )
-    // {
-    //     greeting_message.innerHTML = "Good Afternoon";
-    // }
-    
-    // if( hours >= 18 && hours <= 24 )
-    // {
-    //     greeting_message.innerHTML = "good evening";
-    // }
-
+    //  AM and PM
+    const AM_PM = ( hours >= 12 ) ? session.innerHTML = "PM": session.innerHTML= "AM";          
  }
+
+ // adding zero infront of the numbers below 10
+ const string        = String(hours).length ;
+ const leftHourZero  = document.getElementById('hours').innerHTML = `"0" ${hours}`;
+ const noZeroHour    = document.getElementById('hours').innerHTML = `${hours}`;         
+ const leftZeroHours = (string < 2)   ?  leftHourZero : noZeroHour;
 
 //  calling the displayTime function
  displayTime();
@@ -90,99 +63,8 @@ updateCalendar();
 
 // Again, watch out for how you are declaring variables here.. This may work fine but it is bad practice
 // We should always explicitly define wether a variable is mutable (let) or immutable (const)
-  const AM_PM = ( hours >= 12 ) ? session.innerHTML = "PM": session.innerHTML= "AM";
+  
 
 
  // we are delcaring this if statement in the global scope so the return value isn't being stored anywhere
  // This is what is causing your console error for 'Illegal Return Statement"
- //  current_value is a number
-
-//  function updateTime(current_value)
-//  {
-//     current_value.toString().length <2 ? return "0" + current_value : return current_value;
-//  }
-
-
-
-
-     // creating date function
-
-
-
-// // Declare variables from the html document
-// const current_time      = document.getElementById('current-time');
-// const greeting_message = document.getElementsByClassName('greeting-text')[0];
-// const calendar_display  = document.getElementById("calendar-text");
-
-
-// const date = new Date(); 
-// let hours = date.getHours();
-// let minutes = date.getMinutes();
-// let seconds = date.getSeconds();
-
-// function clock()
-// {
-//     let AM_PM;
-    
-//     // hours = updateTime(hours);
-//     // minutes = updateTime(minutes); 
-//     // seconds = updateTime(seconds);
-
-//     // making the greeting message say "good morning!" from 6am until 1pm
-
-//     AM_PM = ( hours >= 12 ) ? "PM" : "AM";
-//     current_time.innerHTML = `${hours}:${minutes}:${seconds} ${AM_PM}`;
-
-//     setInterval(clock, 10);
-// }
-
-// if ( hours < 12 )
-// {
-//     greeting_message.innerHTML = "Good Morning";
-// }
-
-// if( hours >= 12 && hours <= 18 )
-// {
-//     greeting_message.innerHTML = "Good Afternoon";
-// }
-
-// if( hours >= 18 && hours <= 24 )
-// {
-//     greeting_message.innerHTML = "good evening";
-// }
-
-// current_value is a number
-// function updateTime(current_value)
-// {
-
-//      current_value.toString().length <2 ? return "0" + current_value : return current_value;
-    
-    
-// //     // instead of checking based on numeric value, 
-// //     // we should check based on number of characters
-// //     // ie. instead of 1 < 10 => '01', 
-// //     // if (String(current_value).length < 2 => 01
-// //     if (String(current_value).length < 2)
-// //     {
-// //         const value = '0' + current_value;
-// //         console.log(typeof(value));
-// //         return value
-// //     } 
-// //     else 
-// //     {
-// //     //     return current_value;
-// //     }
-
-// // }
-
-//     // creating date function
-// function updateCalendar()
-// {
-//     const new_date = new Date();
-//     calendar_display.innerHTML = new_date.toLocaleDateString();
-//     // setTimeout(updateCalendar(), 1000);
-// }
-
-// // calling date function
-// clock();
-// updateCalendar();
