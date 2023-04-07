24f98ec63a5712aefd2e9aea1ce956fb6e7bb84a
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-04-07 09:47:39 -0230

    live clock with leading zero and am/pm functioning

--- functions.js
+++ functions.js
@@ -20,51 +20,22 @@ const clock_text       = document.getElementById('clock');
 // Displaying live clock on the top of the page
 function displayTime() 
  {
-    var dateTime     = new Date();
-    var hours        = dateTime.getHours();
-    var minutes      = dateTime.getMinutes();
-    var seconds      = dateTime.getSeconds();
-
-    function liveClock ()
-     {
-        const new_date = new Date();
-        clock_text.innerHTML = new_date.toLocaleTimeString();
-     }
-     setInterval(liveClock, 1000);
-
-    //  AM and PM
-    const AM_PM = ( hours >= 12 ) ? session.innerHTML = "PM": session.innerHTML= "AM";          
+      var dateTime = new Date();
+      var hours = dateTime.getHours().toString().padStart(2, '0');
+      var minutes = dateTime.getMinutes().toString().padStart(2, '0');
+      var seconds = dateTime.getSeconds().toString().padStart(2, '0');
+      var amOrPm = (hours >= 12) ? 'PM' : 'AM';
+      var timeString = hours + ':' + minutes + ':' + seconds + ' ' + amOrPm;
+     clock_text.innerHTML = timeString;
  }
-
- // adding zero infront of the numbers below 10
- const string        = String(hours).length ;
- const leftHourZero  = document.getElementById('hours').innerHTML = `"0" ${hours}`;
- const noZeroHour    = document.getElementById('hours').innerHTML = `${hours}`;         
- const leftZeroHours = (string < 2)   ?  leftHourZero : noZeroHour;
-
-//  calling the displayTime function
- displayTime();
+ setInterval(displayTime, 1000);
 
  function updateCalendar()
 {
     const new_date = new Date();
     calendar_text.innerHTML = new_date.toLocaleDateString();
 }
-
-// calling date function
-
 updateCalendar();
 
  // If you are trying to get your functions working but not seeing anything happening
  // you can try invoking the function in a console log to see what it's return is
-
-//  console.log(displayTime())
-// In this case, the function isn't doing any work or modifying the DOM in any way
-
-// Again, watch out for how you are declaring variables here.. This may work fine but it is bad practice
-// We should always explicitly define wether a variable is mutable (let) or immutable (const)
-  
-
-
- // we are delcaring this if statement in the global scope so the return value isn't being stored anywhere
- // This is what is causing your console error for 'Illegal Return Statement"
